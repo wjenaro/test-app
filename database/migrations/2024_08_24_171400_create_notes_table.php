4e4ff16d4b1fa0ba10dc0357c0dbce1acb8a6444
Jenaro Wachira <wjenaro@gmail.com>
2024-08-24 20:34:47 +0300

    migrate

--- database/migrations/2024_08_24_171400_create_notes_table.php
+++ database/migrations/2024_08_24_171400_create_notes_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
+            $table-> longText('note');
+            $table-> foreignId('user_is') ->constrained('users');
             $table->timestamps();
         });
     }
